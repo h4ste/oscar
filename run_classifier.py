Attempted fix for numerical instability

run_classifier.py
@@ -699,12 +699,18 @@ def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
 
         logits = tf.matmul(output_layer, output_weights, transpose_b=True)
         logits = tf.nn.bias_add(logits, output_bias)
-        probabilities = tf.nn.softmax(logits, axis=-1)
-        log_probs = tf.nn.log_softmax(logits, axis=-1)
 
-        one_hot_labels = tf.one_hot(labels, depth=num_labels, dtype=tf.float32)
+        # m = tf.reduce_max(logits, axis=-1, keepdims=True)
 
-        per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
+        probabilities = tf.nn.softmax(logits, axis=-1) + 1e-6
+        # log_probs = tf.nn.log_softmax(logits - m, axis=-1) + m
+
+        # one_hot_labels = tf.one_hot(labels, depth=num_labels, dtype=tf.float32)
+
+        # per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
+        # loss = tf.reduce_mean(per_example_loss)
+
+        per_example_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
         loss = tf.reduce_mean(per_example_loss)
 
         return (loss, per_example_loss, logits, probabilities)